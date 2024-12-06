affa58281a7481c263403e1411a8a4f7b1842b21
KIRAN C <89858682+Kiranc9901@users.noreply.github.com>
2024-12-06 19:00:22 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -1,1 +1,7 @@
+df = spark.read.csv("dbfs:/FileStore/shared_uploads/kiranc9901@gmail.com/Order_s5aZL68ifM_4SdQ5AaMkl.csv", header=True, inferSchema=True, sep =',')
+df.printSchema()
+df.show()
 
+#databricks function
+
+display(df)
